ffc846f2120b600fe29b285bcfdd4a77f188c96c
dhiraj19999 <dhirajgarad46@gmail.com>
2026-03-25 07:43:27 +0530

    new-commit

--- client/smartweb/src/App.jsx
+++ client/smartweb/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <div>
         <Toaster position="top-right" reverseOrder={false} />
         <Navbar />
-        <Contact/>
+        
 
         <Routes>
           <Route path="/" element={<Home />} />
