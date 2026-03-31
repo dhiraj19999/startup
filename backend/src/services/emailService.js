cc03a4326f37d3390d592cf59402cd36aec3b583
dhiraj19999 <dhirajgarad46@gmail.com>
2026-03-31 14:20:23 +0530

    gmail-service

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -4,17 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false
-  },
-  family: 4   // ⭐ important fix
 });
 
 export const sendEmails = async (data) => {
