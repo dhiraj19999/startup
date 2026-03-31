b4d518e7008911c93b0b076579e1ab94121cf9d6
dhiraj19999 <dhirajgarad46@gmail.com>
2026-03-31 12:25:55 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import dotenv from "dotenv";
 import connectDB from "./src/config/db.js";
 import app from "./src/app.js";
+import cors from "cors";
+
+app.use(cors({
+  origin: "*", // Allow all origins (you can specify your frontend URL here)
+}));
 
 dotenv.config();
 connectDB();
--- client/smartweb/src/components/Contact.jsx
+++ client/smartweb/src/components/Contact.jsx
@@ -37,7 +37,7 @@ export default function Contact() {
   });
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://startup-8adv.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
