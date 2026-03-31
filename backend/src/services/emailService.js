419a3bf14e54e9a88679c45f94a895f0b9bd70c0
dhiraj19999 <dhirajgarad46@gmail.com>
2026-03-31 17:06:43 +0530

    resend-added

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,9 +1,10 @@
 import nodemailer from "nodemailer";
+import { Resend } from 'resend';
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
+/*const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
@@ -29,6 +30,28 @@ export const sendEmails = async (data) => {
       to: mail.trim(),
       subject: "New Contact Message",
       html,
+    });
+  }
+};
+*/
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmails = async (data) => {
+  const receivers = process.env.EMAIL_RECEIVERS.split(",");
+
+  for (const mail of receivers) {
+    await resend.emails.send({
+      from:  process.env.EMAIL_USER, // verified domain or Resend address
+      to: mail.trim(),
+      subject: "New Contact Message",
+      html: `
+        <h2>New Client Contact Message from SmartwebAI</h2>
+        <p><b>Name:</b> ${data.fullName}</p>
+        <p><b>Email:</b> ${data.email}</p>
+        <p><b>Phone:</b> ${data.phone}</p>
+        <p><b>Message:</b> ${data.message}</p>
+      `,
     });
   }
 };
